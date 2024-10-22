954fda78aaa2cde1a0c1216efc6441850d82178a
CodeBlower <saifulrmail@gmail.com>
2024-10-22 22:28:19 +0600

    ssd

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
+
 class AuthController extends Controller
 {
     public function __construct()
